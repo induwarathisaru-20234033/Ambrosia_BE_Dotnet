3bfc85705e641f5809fdff7dd79ba9daefc1033c
Sanketh Kudahetti <sankeithabilash@gmail.com>
2026-02-08 17:01:58 +0530

    local commit

--- AMB.Application/Dtos/PermissionGroupDto.cs
+++ AMB.Application/Dtos/PermissionGroupDto.cs
@@ -18,6 +18,6 @@ namespace AMB.Application.Dtos
         public int Id { get; set; }
         public string PermissionCode { get; set; }
         public String Name { get; set; }
-        public string? Description { get; set; }}
+        public string? Description { get; set; }
     }
 }
