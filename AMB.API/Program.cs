8ece6b4bab2a1dd4bda50dd73aa521526c4a0789
induwarathisaru-20234033 <induwara.20234033@iit.ac.lk>
2026-02-21 00:21:54 +0530

    Enable Swagger UI and comment out HTTPS redirection in Program.cs

--- AMB.API/Program.cs
+++ AMB.API/Program.cs
@@ -117,8 +117,9 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
 
