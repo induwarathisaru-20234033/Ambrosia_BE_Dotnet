77abbe4c26bb8b4a87c1219c798e6e36806807d6
induwarathisaru-20234033 <induwara.20234033@iit.ac.lk>
2026-02-20 22:15:37 +0530

    Remove Swagger UI configuration from Program.cs

--- AMB.API/Program.cs
+++ AMB.API/Program.cs
@@ -117,10 +117,6 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ambrosia API V1");
-});
 
 app.UseHttpsRedirection();
 
