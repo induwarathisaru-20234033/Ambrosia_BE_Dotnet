4a796882e21c7b1f6c3776cbef4ca86c6b2b7cac
induwarathisaru <thisaru2269@gmail.com>
2026-03-15 01:07:52 +0530

    Add DTOs for Currency, InventoryItem, and UnitOfMeasure

---
+++ AMB.Application/Dtos/UnitOfMeasureDto.cs
@@ -0,0 +1,9 @@
+namespace AMB.Application.Dtos
+{
+    public class UnitOfMeasureDto
+    {
+        public int Id { get; set; }
+        public string UoM { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
