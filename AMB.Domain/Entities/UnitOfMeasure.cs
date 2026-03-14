b23b47513514d452a6794677efe577f5cc05ac28
induwarathisaru <thisaru2269@gmail.com>
2026-03-15 01:04:57 +0530

    Add inventory item and currency entities with validation

---
+++ AMB.Domain/Entities/UnitOfMeasure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMB.Domain.Entities
+{
+    public class UnitOfMeasure : BaseEntity
+    {
+        public string UoM { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
