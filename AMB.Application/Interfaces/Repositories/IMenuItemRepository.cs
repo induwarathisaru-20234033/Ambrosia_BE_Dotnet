14ba062b5e8bd89b465bebaab41569eb049d5950
Sanketh Kudahetti <sanketh.20234043@iit.ac.lk>
2026-03-15 17:24:57 +0530

    Created IMenuItemRepository

--- AMB.Application/Interfaces/Repositories/IMenuItemRepository.cs
+++ AMB.Application/Interfaces/Repositories/IMenuItemRepository.cs
@@ -1,13 +1,13 @@
-﻿///*using AMB.Domain.Entities;
+﻿using AMB.Domain.Entities;
 
-//namespace AMB.Application.Interfaces.Repositories
-//{
-//    public interface IMenuItemRepository
-//    {
-//        Task<MenuItem?> GetByIdAsync(int id);
-//        Task<List<MenuItem>> GetByCategoryAsync(string category);
-//        Task<List<MenuItem>> SearchAsync(string searchTerm);
-//        Task<bool> CheckAvailabilityAsync(int menuItemId);
-//        Task<List<MenuItem>> GetByIdsAsync(List<int> ids);
-//    }
-//}*/
+namespace AMB.Application.Interfaces.Repositories
+{
+    public interface IMenuItemRepository
+    {
+        Task<MenuItem?> GetByIdAsync(int id);
+        Task<List<MenuItem>> GetByIdsAsync(List<int> ids);
+        Task<List<MenuItem>> GetByCategoryAsync(string category);
+        IQueryable<MenuItem> GetQuery();
+        Task<bool> CheckAvailabilityAsync(int menuItemId);
+    }
+}
