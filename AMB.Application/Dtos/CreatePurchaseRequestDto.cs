1eb04c0161f92ee809c91ffdcd1309430906278e
induwara-20234033 <induwara.20234033@iit.ac.lk>
2026-03-15 18:01:17 +0530

    Add CreatePurchaseRequestDto and UpdatePurchaseRequestDto for purchase request management

---
+++ AMB.Application/Dtos/CreatePurchaseRequestDto.cs
@@ -0,0 +1,20 @@
+﻿namespace AMB.Application.Dtos
+{
+    public class CreatePurchaseRequestDto
+    {
+        public string Description { get; set; } = string.Empty;
+        public string Supplier { get; set; } = string.Empty;
+        public string RequestedBy { get; set; } = string.Empty;
+        public DateTimeOffset RequestedDeliveryDate { get; set; }
+        public bool IsUrgent { get; set; }
+        public List<CreatePurchaseRequestItemDto> PRItems { get; set; } = new();
+    }
+
+    public class CreatePurchaseRequestItemDto
+    {
+        public int LineItemNo { get; set; }
+        public float RequestedQuantity { get; set; }
+        public float Price { get; set; }
+        public int InventoryItemId { get; set; }
+    }
+}
