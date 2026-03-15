19133e20320e6a208919b81b36ec8870ed6fc314
induwara-20234033 <induwara.20234033@iit.ac.lk>
2026-03-15 20:42:49 +0530

    Add Purchase Requests and Purchase Request Items tables with relationships
    - Created migration for PurchaseRequests and PurchaseRequestItems tables.
    - Defined properties and constraints for both tables.
    - Established foreign key relationships between PurchaseRequestItems and InventoryItems, and PurchaseRequestItems and PurchaseRequests.
    - Updated model snapshot to reflect new entities and their configurations.

---
+++ AMB.Infra/Migrations/20260315133613_AddPRandPRItemTables.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AMB.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPRandPRItemTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PurchaseRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PurchaseRequestCode = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Supplier = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RequestedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RequestedDeliveryDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    IsUrgent = table.Column<bool>(type: "bit", nullable: false),
+                    PurchaseRequestStatus = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    DeletedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeletedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PurchaseRequests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PurchaseRequestItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LineItemNo = table.Column<int>(type: "int", nullable: false),
+                    RequestedQuantity = table.Column<float>(type: "real", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    PurchaseRequestId = table.Column<int>(type: "int", nullable: false),
+                    InventoryItemId = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    DeletedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeletedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PurchaseRequestItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PurchaseRequestItems_InventoryItems_InventoryItemId",
+                        column: x => x.InventoryItemId,
+                        principalTable: "InventoryItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PurchaseRequestItems_PurchaseRequests_PurchaseRequestId",
+                        column: x => x.PurchaseRequestId,
+                        principalTable: "PurchaseRequests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseRequestItems_InventoryItemId",
+                table: "PurchaseRequestItems",
+                column: "InventoryItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseRequestItems_PurchaseRequestId",
+                table: "PurchaseRequestItems",
+                column: "PurchaseRequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseRequests_PurchaseRequestCode",
+                table: "PurchaseRequests",
+                column: "PurchaseRequestCode",
+                unique: true);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PurchaseRequestItems");
+
+            migrationBuilder.DropTable(
+                name: "PurchaseRequests");
+        }
+    }
+}
