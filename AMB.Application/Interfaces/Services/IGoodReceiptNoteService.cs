44afeb9cba9ef33c997dbc92c93254d1c4741d4a
HSandanima <himandi.20234070@iit.ac.lk>
2026-03-17 20:29:10 +0530

    Added GetGoodReceiptNotesPagedAsync method to Service interface

--- AMB.Application/Interfaces/Services/IGoodReceiptNoteService.cs
+++ AMB.Application/Interfaces/Services/IGoodReceiptNoteService.cs
@@ -5,5 +5,6 @@ namespace AMB.Application.Interfaces.Services
     public interface IGoodReceiptNoteService
     {
         Task<GoodReceiptNoteDto> CreateGoodReceiptNoteAsync(CreateGoodReceiptNoteDto request);
+        Task<PaginatedResultDto<GoodReceiptNoteDto>> GetGoodReceiptNotesPagedAsync(GoodReceiptNoteFilterRequestDto request);
     }
 }
