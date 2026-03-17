c115d673853b369e8ad00954f30f78bf9942a806
HSandanima <himandi.20234070@iit.ac.lk>
2026-03-17 20:18:03 +0530

    Add GRNItem and GoodReceiptNote entities to represent goods receipt notes and their associated items

---
+++ AMB.Domain/Entities/GRNItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMB.Domain.Entities
+{
+    public class GRNItem : BaseEntity
+    {
+        public int LineItemNo { get; set; }
+        public float ReceivedQuantity { get; set; }
+        public float AcceptedQuantity { get; set; }
+        public float RejectedQuantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Remarks { get; set; } = string.Empty;
+
+        public int PRItemId { get; set; }
+        public virtual PurchaseRequestItem? PRItem { get; set; }
+
+        public int GRNId { get; set; }
+        public GoodReceiptNote? GoodReceiptNote { get; set; }
+    }
+}
