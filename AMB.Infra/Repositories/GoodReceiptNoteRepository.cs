ee6a4ae8543b0aec8b8381348089351213c0052d
HSandanima <himandi.20234070@iit.ac.lk>
2026-03-17 20:18:11 +0530

    Add GoodReceiptNoteRepository to manage Good Receipt Notes and related operations

---
+++ AMB.Infra/Repositories/GoodReceiptNoteRepository.cs
@@ -0,0 +1,39 @@
+using AMB.Application.Interfaces.Repositories;
+using AMB.Domain.Entities;
+using AMB.Infra.DBContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace AMB.Infra.Repositories
+{
+    public class GoodReceiptNoteRepository : IGoodReceiptNoteRepository
+    {
+        private readonly AMBContext _context;
+
+        public GoodReceiptNoteRepository(AMBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GoodReceiptNote> AddAsync(GoodReceiptNote goodReceiptNote)
+        {
+            _context.GoodReceiptNotes.Add(goodReceiptNote);
+            await _context.SaveChangesAsync();
+            return goodReceiptNote;
+        }
+
+        public async Task<List<PurchaseRequestItem>> GetPurchaseRequestItemsByIdsAsync(List<int> prItemIds)
+        {
+            return await _context.PurchaseRequestItems
+                .Include(item => item.PurchaseRequest)
+                .Where(item => prItemIds.Contains(item.Id))
+                .ToListAsync();
+        }
+
+        public async Task<bool> ExistsByNumberAsync(string grnNumber)
+        {
+            return await _context.GoodReceiptNotes
+                .AsNoTracking()
+                .AnyAsync(grn => grn.GRNNumber == grnNumber);
+        }
+    }
+}
