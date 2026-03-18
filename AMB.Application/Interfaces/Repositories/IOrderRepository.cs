7ff95484f34a2f6548776237aa05ad551829e972
Sanketh Kudahetti <sanketh.20234043@iit.ac.lk>
2026-03-18 13:59:41 +0530

    Added UpdateDraftOrderItemsAsync method to IOrderRepository

--- AMB.Application/Interfaces/Repositories/IOrderRepository.cs
+++ AMB.Application/Interfaces/Repositories/IOrderRepository.cs
@@ -17,6 +17,7 @@ namespace AMB.Application.Interfaces.Repositories
         Task<bool> UpdateOrderStatusAsync(int orderId, OrderStatus newStatus, string? reason = null);
         Task<Order?> GetOrderWithDetailsAsync(int id);
         Task<(List<Order> Items, int TotalCount)> SearchOrdersAsync(SearchOrderRequestDto request);
+        Task<bool> UpdateDraftOrderItemsAsync(int orderId, List<OrderItemDto> items);
     }
 
     public class OrderQueryOptions
