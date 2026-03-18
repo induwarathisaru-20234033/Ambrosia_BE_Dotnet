086ec89cabb0527ee5cd0cc79622261283da0978
Sanketh Kudahetti <sanketh.20234043@iit.ac.lk>
2026-03-18 13:54:20 +0530

    Created UpdateDraftOrderValidator

---
+++ AMB.Application/Validators/UpdateDraftOrderValidator.cs
@@ -0,0 +1,15 @@
+﻿using AMB.Application.Dtos;
+using AMB.Application.Interfaces.Repositories;
+using AMB.Domain.Enums;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AMB.Application.Validators
+{
+    public class UpdateDraftOrderValidator : AbstractValidator<UpdateDraftOrderDto>
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        
+    }
+}
