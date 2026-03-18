b1b772fbbc0383d0afb841ed3f5a4a721623b960
Sanketh Kudahetti <sanketh.20234043@iit.ac.lk>
2026-03-18 13:56:51 +0530

    Implemented method UpdateDraftOrderValidator

--- AMB.Application/Validators/UpdateDraftOrderValidator.cs
+++ AMB.Application/Validators/UpdateDraftOrderValidator.cs
@@ -10,6 +10,32 @@ namespace AMB.Application.Validators
     {
         private readonly IServiceProvider _serviceProvider;
 
-        
+        public UpdateDraftOrderValidator(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+
+            RuleFor(x => x.OrderId)
+                .GreaterThan(0).WithMessage("Invalid order ID")
+                .MustAsync(BeDraftOrder).WithMessage("Order must be in Draft status to update");
+
+            RuleFor(x => x.Items)
+                .NotEmpty().WithMessage("Order must have at least one item");
+
+            RuleForEach(x => x.Items)
+                .ChildRules(item =>
+                {
+                    item.RuleFor(i => i.MenuItemId)
+                        .GreaterThan(0).WithMessage("Invalid menu item");
+
+                    item.RuleFor(i => i.Quantity)
+                        .GreaterThan(0).WithMessage("Quantity must be at least 1");
+                    // REMOVED: .LessThanOrEqualTo(100)
+                });
+
+            // No duplicate menu items
+            RuleFor(x => x.Items)
+                .Must(HaveUniqueMenuItems)
+                .WithMessage("Duplicate menu items are not allowed. Please combine quantities.");
+        }
     }
 }
