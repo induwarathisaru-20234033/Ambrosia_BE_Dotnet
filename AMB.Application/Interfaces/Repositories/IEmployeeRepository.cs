494b2de1925bf23770f04b85c74c5115131b4118
julith2001 <julith.20234032@iit.ac.lk>
2026-03-21 11:48:08 +0530

    add methods to update online status and retrieve active waiters in IEmployeeRepository

--- AMB.Application/Interfaces/Repositories/IEmployeeRepository.cs
+++ AMB.Application/Interfaces/Repositories/IEmployeeRepository.cs
@@ -11,6 +11,8 @@ namespace AMB.Application.Interfaces.Repositories
         Task<Employee?> GetByUsernameAsync(string username);
         Task<Employee?> GetByIdAsync(int id);
         Task<Employee?> UpdateAsync(Employee employee);
+        Task<Employee?> UpdateOnlineStatusAsync(int id, bool isOnline);
+        Task<List<Employee>> GetActiveWaitersAsync();
         Task<List<int>> GetExistingRoleIdsAsync(List<int> roleIds);
         Task<List<int>> GetExistingCustomRoleIdsAsync(List<int> customRoleIds);
         Task AssignRolesAsync(int employeeId, List<int> roleIds, List<int> customRoleIds);
