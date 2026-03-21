4a6d96d86c32413e1ac07620aabe775c756c7cb5
vimukthiwaththegama <vwaththegama@gmail.com>
2026-03-21 22:50:52 +0530

    add WastageEntryItem and WastageRecord entities

---
+++ AMB.Domain/Entities/WastageEntryItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMB.Domain.Entities
+{
+    public class WastageEntryItem : BaseEntity
+    {
+        public int ItemNo { get; set; }
+        public string WastageType { get; set; } = string.Empty;
+        public float Quantity { get; set; }
+        public string Reason { get; set; } = string.Empty;
+
+        public int InventoryItemId { get; set; }
+        public virtual InventoryItem? InventoryItem { get; set; }
+
+        public int WastageRecordId { get; set; }
+        public virtual WastageRecord? WastageRecord { get; set; }
+    }
+}
---
+++ AMB.Domain/Entities/WastageRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMB.Domain.Entities
+{
+    public class WastageRecord : BaseEntity
+    {
+        public string WastageEntryNumber { get; set; }  = string.Empty;
+        public DateTimeOffset EntryDate { get; set; }
+        public string RecordedBy { get; set; } = string.Empty;
+        public string GeneralNotes { get; set; } = string.Empty;
+
+        public virtual ICollection<WastageEntryItem> WastageEntryItems { get; set; } = new List<WastageEntryItem>();
+    }
+}
