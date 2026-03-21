dd428324cdcfe8eaa86045e916d1ee6c06ff973b
julith2001 <julith.20234032@iit.ac.lk>
2026-03-21 16:06:19 +0530

    add GIItem and GoodIssueNote entities for inventory management

---
+++ AMB.Domain/Entities/GIItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMB.Domain.Entities
+{
+    public class GIItem : BaseEntity
+    {
+        public int LineItemNo { get; set; }
+        public float RequestedQuantity { get; set; }
+        public float IssuedQuantity { get; set; }
+        public string IssuedFrom { get; set; } = string.Empty;
+        public string IssuedTo { get; set; } = string.Empty;
+        public string Remarks { get; set; } = string.Empty;
+
+        public int InventoryItemId { get; set; }
+        public virtual InventoryItem? InventoryItem { get; set; }
+
+        public int GoodIssueNoteId { get; set; }
+        public virtual GoodIssueNote? GoodIssueNote { get; set; }
+    }
+}
---
+++ AMB.Domain/Entities/GoodIssueNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMB.Domain.Entities
+{
+    public class GoodIssueNote : BaseEntity
+    {
+        public string GINumber { get; set; } = string.Empty;
+        public string IssuedBy { get; set; } = string.Empty;
+        public DateTimeOffset IssuedDate {  get; set; }
+        public virtual ICollection<GIItem> GIItems { get; set; } = new List<GIItem>();
+    }
+}
