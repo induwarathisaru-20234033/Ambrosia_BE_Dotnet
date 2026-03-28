e9400c0404bc31145b561be963412ce02974b7b2
Sanketh Kudahetti <sanketh.20234043@iit.ac.lk>
2026-03-29 00:20:29 +0530

    Added ReservationId to Order Entity

--- AMB.Domain/Entities/Order.cs
+++ AMB.Domain/Entities/Order.cs
@@ -12,12 +12,15 @@ namespace AMB.Domain.Entities
 
         public int? TableId { get; set; }
 
+        public int? ReservationId { get; set; }
+
         public int OrderStatus { get; set; } = (int)AMB.Domain.Enums.OrderStatus.Draft;
 
         public DateTimeOffset? SentToKitchenAt { get; set; }
 
         // Navigation properties
         public Table? Table { get; set; }
+        public Reservation? Reservation { get; set; }
         public List<OrderItem> OrderItems { get; set; } = new();
     }
 }
