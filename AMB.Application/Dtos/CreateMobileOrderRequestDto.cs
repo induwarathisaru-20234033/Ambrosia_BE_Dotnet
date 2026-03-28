73d6c71e2fe6f4a19f281c1527b01cd277f14618
Sanketh Kudahetti <sanketh.20234043@iit.ac.lk>
2026-03-29 00:44:11 +0530

    Removed mobile order unnecessary files

--- AMB.Application/Dtos/CreateMobileOrderRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AMB.Application.Dtos
-{
-    public class CreateMobileOrderRequestDto
-    {
-        public int TableId { get; set; }
-        public List<OrderItemDto> Items { get; set; } = new();
-        public bool IsDraft { get; set; } = false;
-    }
-}
