00b9c6bed28041525fb7dcc19d70c0f0f46cad3a
Sanketh Kudahetti <sanketh.20234043@iit.ac.lk>
2026-03-29 00:23:32 +0530

    Created CreateMobileOrderRequestDto

---
+++ AMB.Application/Dtos/CreateMobileOrderRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AMB.Application.Dtos
+{
+    public class CreateMobileOrderRequestDto
+    {
+        public int TableId { get; set; }
+        public List<OrderItemDto> Items { get; set; } = new();
+        public bool IsDraft { get; set; } = false;
+    }
+}
